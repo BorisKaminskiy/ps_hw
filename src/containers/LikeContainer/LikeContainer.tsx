a784c47dea9f21e93a23980535f3cb766d36e30b
Boris Kaminskiy <bk-ops-1@bk.ru>
2024-01-01 17:07:06 +0300

    add commit

--- src/containers/LikeContainer/LikeContainer.tsx
+++ src/containers/LikeContainer/LikeContainer.tsx
@@ -34,9 +34,6 @@ export const LikeContainer: FC<ILikeContainerProps> = ({
       .catch((error) => console.error(error))
       .finally(() => setPortal(false));
   };
-  // checkId(blogId);
-  // console.log(checkId(blogId));
-  // setId(blogId);
 
   return (
     <div className={cn(styles.root)}>
